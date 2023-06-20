df75179acf0d87cc71f1502d4299abd368827887
theazammohamed <theazammohamed@gmail.com>
2023-06-20 15:27:16 +0530

    updating all files

--- couply/script.js
+++ couply/script.js
@@ -1,3 +1,6 @@
 document.querySelector(".menu").addEventListener("click", function() {
   document.querySelector(".center").classList.toggle("slide-in");
 });
+document.querySelector(".close").addEventListener("click", function() {
+  document.querySelector(".center").classList.replace("slide-in", "slide-out");
+});
