365738596edf81189b9d127f20bfef730a1ea4fe
theazammohamed <theazammohamed@gmail.com>
2024-04-18 18:04:25 +0530

    marquee text successfully configured

--- Portfolio/JS/minorInteractions.js
+++ Portfolio/JS/minorInteractions.js
@@ -64,3 +64,27 @@ openNav.addEventListener("click", () => {
 closeNav.addEventListener ("click", () => {
     open.timeScale(4).reverse();
 })
+
+
+// Gemini Marquee test
+let currentScroll = 0;
+let isScrollingDown = true;
+
+let tween = gsap.to(".marquee__part", {xPercent: -100, repeat: -1, duration: 10, ease: "linear"}).totalProgress(0.5);
+
+gsap.set(".marquee__inner", {xPercent: -50});
+
+window.addEventListener("scroll", function(){
+  
+  if ( window.pageYOffset > currentScroll ) {
+    isScrollingDown = true;
+  } else {
+    isScrollingDown = false;
+  }
+   
+  gsap.to(tween, {
+    timeScale: isScrollingDown ? 1 : -1
+  });
+  
+  currentScroll = window.pageYOffset
+});
