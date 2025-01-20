ec3aa1e5e0003a04f21bbc3d93dd091d4b1f029d
hello.indoortime@gmail.com <theazammohamed@gmail.com>
2025-01-20 11:08:28 +0530

    Initialized react app with sass installed

--- src/sections/Landing/index.js
+++ src/sections/Landing/index.js
@@ -1,10 +1,12 @@
-
+import './index.scss'
 
 function Landing() {
   return (
-    <div>
-        Hello Lander!
-    </div>
+    <section id='landing'>
+        <div className='imageWrap'>
+            <img alt="River-and-mountains-of-snow" src="https://images.unsplash.com/photo-1480497490787-505ec076689f?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        </div>
+    </section>
   );
 }
 
